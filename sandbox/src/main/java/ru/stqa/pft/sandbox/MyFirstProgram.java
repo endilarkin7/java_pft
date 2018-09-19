e86331706801974a2bf4a002371a0479fd571c59
Vitalik <vetal.t28@gmail.com>
2018-09-19 23:17:45 +0300

    23:17

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,21 +1,23 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.Arrays;
+
 public class MyFirstProgram {
-	public static void main (String[] args) {
-		hello("Alex");
-		String somebody = "world";
-		System.out.println("Hello, " + somebody);
-		double l = 8.0;
-		double s = l * l;
-		System.out.println("Площа квадрата зі стороною " + l + " = " + area(l));
-	}
-	public static void hello ( String somebody) {
-		System.out.println("Hello " + somebody);
-	}
-	public static double area ( double len) {
-		return len * len;
-	}
-	public  static double area (double a, double b) {
-		return a * b;
-	}
-}
+
+  public static void main(String[] args) {
+      String studentName = "john";
+      int score = 90;
+      String grade;
+
+      if (score >= 90 && score <= 100) {
+        grade = "A";
+      } else if (score >= 80 && score < 90) {
+        grade = "B";
+      } else if (score >= 70 && score < 80) {
+        grade = "C";
+      } else {
+        grade = "D";
+      }
+
+  //  System.out.println("Grade of" + studentName + grade);
+    }}
