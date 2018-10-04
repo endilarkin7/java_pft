6a106517891081e3cc3be875e6434454d5b45818
Vitalik <vetal.t28@gmail.com>
2018-10-04 18:47:41 +0300

    40 лекция

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/GroupCreationTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.Tests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.model.GroupData;
 
@@ -8,8 +9,10 @@ public class GroupCreationTest extends TestBase {
 
     @Test
     public void testGroupCreation() {
-
-        app.getNavigationHelper().gotoGroupPage();  // Перейти на страницу списка групп
-        app.getGroupHelper().createGroup(new GroupData(null, "test2", "test3"));  // Создать новую группу
+        app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount(); // к-сть групп ДО добавления
+        app.getGroupHelper().createGroup(new GroupData(null, "test2", "test3"));
+        int after = app.getGroupHelper().getGroupCount(); // к-сть групп ПОСЛЕ добавление
+        Assert.assertEquals(after,before + 1);
     }
 }
