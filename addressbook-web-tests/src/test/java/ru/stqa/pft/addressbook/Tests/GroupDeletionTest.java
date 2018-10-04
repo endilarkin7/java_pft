b9345d7cb0ed765a0bb50d1e121be14696ad72d9
Vitalik <vetal.t28@gmail.com>
2018-10-04 18:52:21 +0300

    40 лекция

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/GroupDeletionTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.Tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.model.GroupData;
 
@@ -10,10 +11,13 @@ public class GroupDeletionTest extends TestBase{
     @Test
     public void GroupDeletionTest() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()) { //Щоб удалити тест, треба щоб він був, тому створюємо
         app.getGroupHelper().createGroup(new GroupData("test1",null,null));
         }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returntoGroupPage();
+        int after = app.getGroupHelper().getGroupCount(); // к-сть групп ПОСЛЕ добавление
+        Assert.assertEquals(after,before - 1);
     } }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/Tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.Tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.model.GroupData;
 
@@ -8,6 +9,7 @@ public class GroupModificationTests extends TestBase{
 @Test
     public void  testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) { //Щоб удалити тест, треба щоб він був, тому створюємо
         app.getGroupHelper().createGroup(new GroupData("test1",null,null));
     }
@@ -15,5 +17,9 @@ public class GroupModificationTests extends TestBase{
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData(null, "test2", "test3"));  // Заполнить форму какимыта данными
     app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returntoGroupPage();
+    int after = app.getGroupHelper().getGroupCount(); // к-сть групп ПОСЛЕ добавление
+    Assert.assertEquals(after,before);
+
 }
 }
